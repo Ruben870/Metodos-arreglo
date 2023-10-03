5156adac9a597911286731e62356fa26716956b2
Ruben870 <127579907+Ruben870@users.noreply.github.com>
2023-10-03 14:35:41 -0700

    Final

---
+++ topicos/script.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ *const array = [3,6,1,9,10,5];
+
+const numMayor = array.filter((num)=>num>=5);
+
+numMayor.forEach((e) => console.log(e));
+ */
+
+
+
+//const frutas=['naranja','platano','manzana','fresa','kiwi'];
+
+//console.log(frutas.slice(2, 4));
+
+
+
+/**
+ * const array = [20, 5, 17, 52, 34, 88];
+
+array.map((num, index)=>{
+    console.log(index+":"+(num+10));
+});
+ */
+
+
+/**
+ *const array1 = ['H','I','J'];
+const array2 = ['K','L','M'];
+
+const array3 = array1.concat(array2);
+
+console.log(array3); 
+ */
+
+
+const paises = ['Mexico','USA','Francia'];
+
+console.log(paises.includes('Francia'));
+
+
+
+
